9632ea46801a98eceabd95c683dc761237446a11
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-02-26 15:44:55 +0530

    Redid

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ st.write("This website is still under constrution!")
 
 st.header("Here are some of my other websites:-")
 
-st.link_button("SockText (also under construction)", "https://sk-zubair-ahmed.vercel.app")
+st.link_button("SockText (also under construction)", "https://sock-text.vercel.app")
+st.link_button("My website", "https://sk-zubair-ahmed.vercel.app")
