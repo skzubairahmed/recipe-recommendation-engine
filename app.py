fb5ddcf5a2340fcc4f27cf92c0129ef75925b43f
Sk Zubair Ahmed <sheikhzubairahmed3949@gmail.com>
2026-03-05 19:59:08 +0530

    Initial commit

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.title("Recipe Recommendation Engine")
-st.write("This website is still under constrution!")
-
-st.header("Here are some of my other websites:-")
-
-st.link_button("SockText (also under construction)", "https://sock-text.vercel.app")
-st.link_button("My website", "https://sk-zubair-ahmed.vercel.app")
